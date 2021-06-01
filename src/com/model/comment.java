9321e273b3a3dc1f54b1f655c0e5bd520ba8e412
Peng <18130504973@163.com>
2021-06-01 21:42:05 +0800

    model

---
+++ src/com/model/comment.java
@@ -0,0 +1,49 @@
+package com.model;
+
+public class comment {
+    private int id;
+    private String username;
+    private String title;
+    private String content;
+    private String pubtime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPubtime() {
+        return pubtime;
+    }
+
+    public void setPubtime(String pubtime) {
+        this.pubtime = pubtime;
+    }
+}
---
+++ src/com/model/user.java
@@ -0,0 +1,58 @@
+package com.model;
+
+public class user {
+    private String username;
+    private String realname;
+    private String password;
+    private String gender;
+    private String email;
+    private String is_user;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIs_user() {
+        return is_user;
+    }
+
+    public void setIs_user(String is_user) {
+        this.is_user = is_user;
+    }
+}
